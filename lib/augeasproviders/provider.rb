2ab6e65cf53d45befcdafdff907aa3c8f4bbef14
Raphaël Pinson <raphael.pinson@camptocamp.com>
2013-07-22 10:54:04 +0200

    Define class and instance methods for attr_aug_reader_* and attr_aug_writer_* methods

--- lib/augeasproviders/provider.rb
+++ lib/augeasproviders/provider.rb
@@ -194,8 +194,9 @@ module AugeasProviders::Provider
 
       rpath = label.nil? ? '$resource' : "$resource/#{label}"
 
-      # Getter method using an existing aug handler and a path
-      define_method("attr_aug_reader_#{name}") do |aug, *args|
+      # Class getter method using an existing aug handler
+      # FIXME: we're sending to the wrong class (but it works)
+      self.class.send(:define_method, "attr_aug_reader_#{name}") do |aug, *args|
         case type
         when :string
           aug.get(rpath)
@@ -220,6 +221,11 @@ module AugeasProviders::Provider
         else
           fail "Invalid type: #{type}"
         end
+      end
+
+      # Instance getter method for the instance
+      define_method("attr_aug_reader_#{name}") do |aug, *args|
+        self.class.send("attr_aug_reader_#{name}", aug, *args)
       end
 
       # We are calling the resource's augopen here, not the class
@@ -249,8 +255,9 @@ module AugeasProviders::Provider
 
       rpath = label.nil? ? '$resource' : "$resource/#{label}"
 
-      # Getter method using an existing aug handler and a path
-      define_method("attr_aug_writer_#{name}") do |aug, *args|
+      # Class setter method using an existing aug handler
+      # FIXME: we're sending to the wrong class (but it works)
+      self.class.send(:define_method, "attr_aug_writer_#{name}") do |aug, *args|
         aug.rm("#{rpath}[position() != 1]") if purge_ident
         case type
         when :string
@@ -293,6 +300,11 @@ module AugeasProviders::Provider
         else
           fail "Invalid type: #{type}"
         end
+      end
+
+      # Instance setter method for the instance
+      define_method("attr_aug_writer_#{name}") do |aug, *args|
+        self.class.send("attr_aug_writer_#{name}", aug, *args)
       end
 
       # We are calling the resource's augopen here, not the class
